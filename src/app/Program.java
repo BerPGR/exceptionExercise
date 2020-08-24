bb5c6bf02be1881e1585354abeed7110508d14ad
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2020-08-24 10:40:17 -0300

    Solução exercício de excessões

---
+++ src/app/Program.java
@@ -0,0 +1,45 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data: ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Name: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		System.out.println();
+		
+		try {
+			System.out.print("Enter value to withdraw: ");
+			double amount = sc.nextDouble();
+			acc.withdraw(amount);
+			System.out.println("New balance: R$" + String.format("%.2f", acc.getBalance()));
+		}
+		catch(DomainException e) {
+			System.out.print("Withdraw error: " + e.getMessage());
+		}
+		catch(RuntimeException e) {
+			System.out.println("unexpected error");
+		}
+		
+		
+		sc.close();
+	}
+
+}
